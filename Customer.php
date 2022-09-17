3bd84af67355763135f4896bc8d02493721f438f
syachrilRamadhan <syachrilbls99@gmail.com>
2022-09-18 02:31:40 +0700

    Contoh Library Composer

---
+++ Customer.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+namespace SyachrilRamadhan;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name):string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
